4cd94e85a8027bdf6f27aa4a4c723f34a29ea89d
philip howley <howley.phil@gmail.com>
2022-04-15 18:14:02 -0400

    Add rockstar status I will miss yall

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+  movies (movie_name)
+VALUES
+  ("Angry Man"),
+  ("My Name"),
+  ("The Song of Names");
+INSERT INTO
+  reviews (movie_id, review)
+VALUES
+  (001, "Good"),
+  (002, "Exellent"),
+  (003, "Bad");
